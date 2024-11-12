31485ca045e0561097d4e78a156765073d20b31a
Trevor <galentrevor@gmail.com>
2024-11-12 07:49:59 -0500

    Added admin page and some fake links for now

---
+++ oldPeopleManagementSystem/resources/views/admin.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Homepage</title>
+</head>
+<body>
+<div class="container" style="margin-top: 20px;">
+<h2>Welcome to Admin Homepage</h2>
+<hr>
+<a href="make_appointment" class="button-class">Schedule appointments for patients</a>
+<a href="employees" class="button-class">View Employees</a>
+<a href="payment" class="button-class">Manage Payments</a>
+<a href="aproval" class="button-class">Aprove New Users</a>
+<a href="roster" class="button-class">View the roster</a>
+<a href="create_roster" class="button-class">Create a Roster</a>
+</body>
+</html>
