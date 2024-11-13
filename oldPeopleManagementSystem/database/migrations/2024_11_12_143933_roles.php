63d07ee1e9854c53a3bb352890b4bce6fd6114b9
JustAHoneySlime <epfriedrichs3@gmail.com>
2024-11-13 07:56:57 -0500

    Finished role page basic functionality
    Still require prevention for users that are not admin

--- oldPeopleManagementSystem/database/migrations/2024_11_12_143933_roles.php
+++ oldPeopleManagementSystem/database/migrations/2024_11_12_143933_roles.php
@@ -6,27 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin_pages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('role');
+            $table->integer('access_lvl');
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin_pages');
     }
 };
