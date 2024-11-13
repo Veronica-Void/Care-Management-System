39768553708aa6281f215cf7fd43b626ebd4a74d
JustAHoneySlime <epfriedrichs3@gmail.com>
2024-11-13 08:09:22 -0500

    Added some comments

--- oldPeopleManagementSystem/app/Http/Controllers/AdminPageController.php
+++ oldPeopleManagementSystem/app/Http/Controllers/AdminPageController.php
@@ -8,14 +8,22 @@ use Illuminate\Http\Request;
 class AdminPageController extends Controller
 {
     //Use $adminPage
+    // Grabs all the data from the admin_pages table and sends it to the page
     public function role()
     {
         $roles = AdminPage::all();
+        if (Session::get('role') !== 'admin') {
+            return back()->with('fail','You must be an admin');
+        }
         return view("auth.role", compact("roles"));
     }
 
+    // Adds a new role to the admin_pages table and sends that data to the page
     public function makeRole(Request $request)
     {
+        if (Session::get('role') !== 'admin') {
+            return back()->with('fail','You must be an admin');
+        }
         $roles = new AdminPage();
         $roles->role = $request->role;
         $roles->access_lvl = $request->access_num;
