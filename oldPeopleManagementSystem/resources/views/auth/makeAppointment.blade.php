0f6db728ae349ebd273ae2e77d5df7863bda97b5
JustAHoneySlime <epfriedrichs3@gmail.com>
2024-11-18 10:22:04 -0500

    Added buttons to each page
    Home needed to be tweaked to each role

--- oldPeopleManagementSystem/resources/views/auth/makeAppointment.blade.php
+++ oldPeopleManagementSystem/resources/views/auth/makeAppointment.blade.php
@@ -4,53 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Make Appointments</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+    <header style="display: block; margin-top: 20px;">
+        @if(Session::get('role') == 'admin')
+            <a href="/admin" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1% 0.5% 0.1% 0.5%; margin-top: 1%; margin-left: 1%; text-decoration: none;">Home</a>
+        @endif
+        <a href="make_appointment" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Schedule appointments for patients</a>
+        <a href="employees" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">View Employees</a>
+        <a href="payment" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Manage Payments</a>
+        <a href="roster" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">View the roster</a>
+        <a href="create_roster" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Create a Roster</a>
+        <a href="/admin/role" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Role page</a>
+        <a href="approval" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Approval page</a>
+        <a href="patients" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Patients page</a>
+        <a href="/additionalPatientInfo" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Patient Info</a>
+        <a href="/make/appointment" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Make Appointment</a>
+    </header>
 <body>
-    <div class="role-container" style="margin-top: 20px;">
-    <h2>Make new appointment</h2>
-    <hr>
-    <form action="{{ route('find_patient') }}" method="post">
-        @if(Session::has('success'))
-        <div class="alert alert-success">{{Session::get('success')}}</div>
-        @endif
-        @if(Session::has('fail'))
-        <div class="alert alert-danger">{{Session::get('fail')}}</div>
-        @endif
-        @csrf
-        <div class="form-group">
-            <p>Patient ID: <input type="number" name="id" min=0><button type="submit">Search</button></p>
+    <div class="container mt-2">
+        <div class="card shadow-sm mb-4">
+        <div class="card-header bg-primary text-white">
+            <h2>Make new appointment</h2>
         </div>
-    </form>
-    <form action="{{ route('makeAppointment') }}" method="post">
-        @if(Session::has('success'))
-        <div class="alert alert-success">{{Session::get('success')}}</div>
-        @endif
-        @if(Session::has('fail'))
-        <div class="alert alert-danger">{{Session::get('fail')}}</div>
-        @endif
-        @csrf
-        <div class="form-group">
-            <p>Current Patient ID:</p><p name="patient_id">{{ $patient_id }}</p>
-            <p>Current Patient Name:</p><p name="patient_name">{{ $patient }}</p>
+        <form action="{{ route('find_patient') }}" class="container mt" style="margin-top: 3%;" method="post">
+            @if(Session::has('success'))
+            <div class="alert alert-success">{{Session::get('success')}}</div>
+            @endif
+            @if(Session::has('fail'))
+            <div class="alert alert-danger">{{Session::get('fail')}}</div>
+            @endif
+            @csrf
+            <div class="form-group">
+                <p>Patient ID: <input type="number" name="id" min=0 ><button type="submit" class="btn btn-primary success-bg-subtle" style="margin-left: 10px;">Search</button></p>
+            </div>
+        </form>
+        <div class="container mt" style="margin-bottom: 3%;">
+            <form action="{{ route('makeAppointment') }}" method="post">
+                @if(Session::has('success'))
+                <div class="alert alert-success">{{Session::get('success')}}</div>
+                @endif
+                @if(Session::has('fail'))
+                <div class="alert alert-danger">{{Session::get('fail')}}</div>
+                @endif
+                @csrf
+                <div class="form-group">
+                    <p>Current Patient ID:</p><p name="patient_id">{{ $patient_id }}</p>
+                    <p>Current Patient Name:</p><p name="patient_name">{{ $patient }}</p>
+                </div>
+                <div class="form-group">
+                    <label for="dob">Date:</label>
+                    <input type="date" class="form-control" name="dob" id="dob" value="{{old('dob')}}">
+                    <span class="text-danger">@error('dob') {{$message}} @enderror</span>
+                </div>
+                <div class="form-group">
+                    <label for="doctor">Doctor:</label>
+                    <select name="doctor" id="doctor" class="form-control">
+                        @foreach ($doctors as $doctor)
+                            <option>{{ $doctor }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="change_role" class="btn btn-primary success-bg-subtle" style="margin-left: 10px; margin-bottom: 10px; margin-top: 3%;">Ok</button>
+            </form>
+            <a href="role"><button type="cancel" class="btn btn-primary" style="margin-left: 10px; margin-bottom: 10px;">Cancel</button></a>
         </div>
-        <div class="form-group">
-            <label for="dob">Date:</label>
-            <input type="date" class="form-control" name="dob" id="dob" value="{{old('dob')}}">
-            <span class="text-danger">@error('dob') {{$message}} @enderror</span>
-        </div>
-        <div class="form-group">
-            <label for="doctor">Doctor:</label>
-            <select name="doctor" id="doctor" class="form-control">
-                @foreach ($doctors as $doctor)
-                    <option>{{ $doctor }}</option>
-                @endforeach
-            </select>
-        </div>
-        <button>Ok</button>
-    </form>
-    <a href="appointment"><button type="cancel">Cancel</button></a>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- oldPeopleManagementSystem/resources/views/patients.blade.php
+++ oldPeopleManagementSystem/resources/views/patients.blade.php
@@ -6,6 +6,21 @@
     <title>Patients page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+    <header style="display: block; margin-top: 20px;">
+        @if(Session::get('role') == 'admin')
+            <a href="/admin" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1% 0.5% 0.1% 0.5%; margin-top: 1%; margin-left: 1%; text-decoration: none;">Home</a>
+        @endif
+        <a href="make_appointment" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Schedule appointments for patients</a>
+        <a href="employees" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">View Employees</a>
+        <a href="payment" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Manage Payments</a>
+        <a href="roster" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">View the roster</a>
+        <a href="create_roster" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Create a Roster</a>
+        <a href="/admin/role" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Role page</a>
+        <a href="approval" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Approval page</a>
+        <a href="patients" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Patients page</a>
+        <a href="/additionalPatientInfo" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Patient Info</a>
+        <a href="/make/appointment" class="bg-primary-subtle text-black" style="border-radius: 10px; border: solid; border-color: black; text-align: center; width: 10%; padding: 0.1%; margin-top: 1%; text-decoration: none;">Make Appointment</a>
+    </header>
 <body>
 <table class="table">
         <thead>
