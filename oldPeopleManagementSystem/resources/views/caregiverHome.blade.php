10e0b337e5d8f51e16da3f7ebd411f49716dc1ed
TrevorG03 <117454281+TrevorG03@users.noreply.github.com>
2024-11-20 14:58:04 -0500

    Revert "Ethan"

--- oldPeopleManagementSystem/resources/views/caregiverHome.blade.php
+++ oldPeopleManagementSystem/resources/views/caregiverHome.blade.php
@@ -9,8 +9,7 @@
 
 <body>
     <h2>Caregiver Homepage</h2>
-    <form action="{{ route('getPatient') }}" method="post">
-        
+    <form action="find_patient" method="post">
         <div class="container mt" style="margin-bottom: 3%;">
             <label for="patient">Patient:</label>
             <select name="patient" id="patient" class="form-control" style="max-width: 25%;">
@@ -27,42 +26,42 @@
             <button type="submit" class="btn btn-primary success-bg-subtle">Search</button>
         </div>
     </form>
-    <form action="{{ route('check') }}" method="post">
+    <form action="caregiver" method="post">
         <div class="card shadow-sm mb-4">
             <div class="container-sm card-header bg-primary text-white">
                 <h3>Checklist</h3>
             </div>
             <div class="container-sm card-header bg-secondary text-white" style="display: table; padding: 10px;">
                 <div style="vertical-align: top; display: table-cell; text-align: center;">
-                    <!-- <p style="background-color: green; border-type: solid; border-radius: 10px;">Complete</p> -->
-                    <input type="checkbox" id="morning_med" value=1>
+                    <p style="background-color: green; border-type: solid; border-radius: 10px;">Checked</p>
+                    <input type="checkbox" id="morning_med">
                     <label for="morning_med">Morning Medicine</label>
                 </div>
                 <div style="vertical-align: top; display: table-cell; text-align: center;">
-                    <input type="checkbox" id="afternoon_med" value=1>
+                    <input type="checkbox" id="afternoon_med">
                     <label for="afternoon_med">Afternoon Medicine</label>
                 </div>
                 <div style="vertical-align: top; display: table-cell; text-align: center;">
-                    <input type="checkbox" id="night_med" value=1>
+                    <input type="checkbox" id="night_med">
                     <label for="night_med">Night Medicine</label>
                 </div>
                 <div style="vertical-align: top; display: table-cell; text-align: center;">
-                    <input type="checkbox" id="breakfast" value=1>
+                    <input type="checkbox" id="breakfast">
                     <label for="breakfast">Breakfast</label>
                 </div>
                 <div style="vertical-align: top; display: table-cell; text-align: center;">
-                    <input type="checkbox" id="lunch" value=1>
+                    <input type="checkbox" id="lunch">
                     <label for="lunch">Lunch</label>
                 </div>
                 <div style="vertical-align: top; display: table-cell; text-align: center;">
-                    <input type="checkbox" id="dinner" value=1>
+                    <input type="checkbox" id="dinner">
                     <label for="dinner">Dinner</label>
                 </div>
             </div>
         </div>
         <a href="" class="btn approve btn-primary success-bg-subtle" style="margin-left: 10px; margin-bottom: 10px; margin-top: 3%;">Confirm</a>
+        <a href="" class="btn approve btn-primary success-bg-subtle" style="margin-left: 10px; margin-bottom: 10px; margin-top: 3%;">Clear</a>
     </form>
-    <a href="" class="btn approve btn-primary success-bg-subtle" style="margin-left: 10px; margin-bottom: 10px; margin-top: 3%;">Clear</a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
